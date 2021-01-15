fbef3d19658c20a4e3f0279477fcbdf30807e5ef
Ilya Kaloshin <ilya.kaloshin@gmail.com>
2021-01-15 16:53:22 +0300

    remove some rules

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = function(md, options) {
   options = options || {};
   options.listUnicodeChar = options.hasOwnProperty('listUnicodeChar') ? options.listUnicodeChar : false;
   options.stripListLeaders = options.hasOwnProperty('stripListLeaders') ? options.stripListLeaders : false;
-  options.gfm = options.hasOwnProperty('gfm') ? options.gfm : true;
   options.useImgAltText = options.hasOwnProperty('useImgAltText') ? options.useImgAltText : true;
 
   var output = md || '';
@@ -14,23 +13,14 @@ module.exports = function(md, options) {
       else
         output = output.replace(/^([\s\t]*)([\*\-\+]|\d+\.)\s+/gm, '$1');
     }
-    if (options.gfm) {
-      output = output
-        // Header
-        .replace(/\n={2,}/g, '\n')
-        // Fenced codeblocks
-        .replace(/~{3}.*\n/g, '')
-        // Strikethrough
-        .replace(/~~/g, '')
-        // Fenced codeblocks
-        .replace(/`{3}.*\n/g, '');
-    }
     output = output
       // Remove emphasis (repeat the line to remove double emphasis)
       .replace(/([\*_]{1,3})(\S.*?\S{0,1})\1/g, '$2')
       .replace(/([\*_]{1,3})(\S.*?\S{0,1})\1/g, '$2')
       // Remove code blocks
       .replace(/(`{3,})(.*?)\1/gm, '$2')
+      // Remove strikethrough
+      .replace(/~~(.+?)~~/g, '$1')
       // Remove inline code
       .replace(/`(.+?)`/g, '$1');
   } catch(e) {
